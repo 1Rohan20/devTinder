633814c41a39e0649e396d106f766229ad096c30
Rohan Gupta <gupta.rohan8907@gmail.com>
2025-08-25 22:51:47 +0530

    changes

--- src/middleware_error.js
+++ src/middleware_error.js
@@ -1,13 +1,28 @@
 const express = require("express")
 const app = express();
 
+//we can set as many route handler as we want to 
+//we can also wrap them in an array 
+//doest make any difference
+
 app.use("/",(req,res,next)=>{
+    console.log("handler 1")
     next()
-    res.send("Route handler 1")  //if we dont do res.send() then an infinite loop happens
+    // res.send("Route handler 1")  //if we dont do res.send() then an infinite loop happens
     // next()
 },(req,res,next)=>{
+    console.log("handler 2")
     next()
-    res.send("route handler 2")   //in this case route handler  1 will be printed
+    // res.send("route handler 2")   //in this case route handler  1 will be printed
+},(req,res,next)=>{
+    console.log("handler 3")
+    next();
+},(req,res,next)=>{
+    console.log("handler 4")
+    next();
+},(req,res,next)=>{
+    console.log("handler 5")
+    res.send("5th handler")
 })
 
 //but if we pass "next" argument in the first handler then we might be able to see route handler 2 . But it depends on the position of next
