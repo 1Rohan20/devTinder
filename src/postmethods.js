5bc9e0ba3ae549301a46a04543d8d26890b445fc
Rohan Gupta <gupta.rohan8907@gmail.com>
2025-08-25 16:13:44 +0530

    bit chnages

--- src/postmethods.js
+++ src/postmethods.js
@@ -6,6 +6,16 @@ app.get("/",(req,res)=>{
     res.send("hello ");
 })
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Rohan",lastName:"Gupta"})
+})
+
+
+app.post("/user",(req,res)=>{
+    //saving data to dataBase
+    console.log("object")
+    res.send("data saved successfully")
+})
 app.listen(7777,()=>{
     console.log("hello from the server")
 })
