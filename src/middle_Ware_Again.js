f6170747a4095926c5c1eb1476ba311879b3ee85
Rohan Gupta <gupta.rohan8907@gmail.com>
2025-08-29 15:38:07 +0530

    changes

--- src/middle_Ware_Again.js
+++ src/middle_Ware_Again.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const app = express()
 
-app.get("/user",(req,res)=>{
-     res.send({firstname:"Rohan",lastname:"Gupta"})
-})
+// app.get("/user",(req,res)=>{
+//      res.send({firstname:"Rohan",lastname:"Gupta"})
+// })
 
-app.listen(3000,()=>{
+
+//a single route can have multiple route handler
+app.use(
+    "/user",
+    (req, res, next) => {
+        //this function is called route handler
+        console.log("handling route 1")
+        // res.send("route handler 1") //if we remove res.send then we get an infinte loop because we havent used NEXT
+        next()                        //adding next() makes sure that our request is forwarded to the next route handler if we havent responded back
+        //    res.send("heolo")       //if we write here then we get error if the nexthandler is responding back
+    },
+    (req, res) => {
+        console.log("handling route 2")
+        res.send("route handler 2")
+    })
+
+app.listen(3000, () => {
     console.log("server up and running")
 })
